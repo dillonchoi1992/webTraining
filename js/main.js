c982d0057c9a97306a5295d3cbfc8dfebeb36a0c
Dillon J. Choi <dillonchoi1992@gmail.com>
2022-03-14 10:36:13 -0400

    intial commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function myTest() {
+    alert("Hello -- I'm javascript and I'm working!");
+    console.log("This is better because it doesn't pop up!");
+}
+
+let str = "Apple, Banana, Kiwi";
+document.getElementById("demoSubstring").innerHTML = str.substring(7,13);
